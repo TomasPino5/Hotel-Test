6ddea65076f05a9899be849daa8b3519889b8167
TomasPino5 <tomaspino.velez@gmail.com>
2023-09-18 19:15:34 -0300

    terminacion del home y galeria

---
+++ client/src/components/home/Galery.jsx
@@ -0,0 +1,34 @@
+import styles from './Galery.module.css'
+import galery1 from '../../img/galery/galery1.jpg'
+import galery2 from '../../img/galery/galery2.jpg'
+import galery3 from '../../img/galery/galery3.jpg'
+import galery4 from '../../img/galery/galery4.jpg'
+import galery5 from '../../img/galery/galery5.jpg'
+import galery6 from '../../img/galery/galery6.jpg'
+import galery7 from '../../img/galery/galery7.jpg'
+import galery8 from '../../img/galery/galery8.jpg'
+import galery9 from '../../img/galery/galery9.jpg'
+import galery10 from '../../img/galery/galery10.jpg'
+import galery11 from '../../img/galery/galery11.webp'
+import galery12 from '../../img/galery/galery12.jpg'
+
+const Galery = () => {
+    return (
+        <div className={styles.galery}>
+            <img className={styles.item} src={galery1} alt="galery1" />
+            <img className={styles.item} src={galery2} alt="galery2" />
+            <img className={styles.item} src={galery3} alt="galery3" />
+            <img className={styles.item} src={galery4} alt="galery4" />
+            <img className={styles.item} src={galery5} alt="galery5" />
+            <img className={styles.item} src={galery6} alt="galery6" />
+            <img className={styles.item} src={galery7} alt="galery7" />
+            <img className={styles.item} src={galery8} alt="galery8" />
+            <img className={styles.item} src={galery9} alt="galery9" />
+            <img className={styles.item} src={galery10} alt="galery10" />
+            <img className={styles.item} src={galery11} alt="galery11" />
+            <img className={styles.item} src={galery12} alt="galery12" />
+        </div>
+    )
+}
+
+export default Galery;
--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,6 +1,13 @@
 import styles from './Home.module.css'
 import Carousel from './Carousel';
 import { Link } from 'react-router-dom';
+import home1 from '../../img/home/home1.jpg'
+import home2 from '../../img/home/home2.jpg'
+import home3 from '../../img/home/home3.jpg'
+import home4 from '../../img/home/home4.jpg'
+import home5 from '../../img/home/home5.jpg'
+import home6 from '../../img/home/home6.jpg'
+import Galery from './Galery';
 
 const Home = () => {
     return (
@@ -45,25 +52,85 @@ const Home = () => {
             </div>
             <div className={styles.items}>
                 <div className={styles.item1}>
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <h1>1</h1>
-                    <p className={styles.description}>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, illo ipsam nobis,     ullam omnis quibusdam voluptatem aut impedit magni assumenda perferendis debitis. Qui     totam ex officia unde nam quo molestias.
-                    </p>
-                    <button 
-                        type='submit' 
-                        className={styles.infoButton} 
-                    >
-                        <Link
-                            to="/habitaciones"
-                            className={styles.link}
+                    <div className={styles.imgs}>
+                        <img className={styles.img1} src={home1} alt="home1" />
+                        <img className={styles.img2} src={home2} alt="home2" />
+                    </div>
+                    <div className={styles.content}>
+                        <h1 className={styles.num}>01</h1>
+                        <h2 className={styles.h2}>Comparte habitación</h2>
+                        <p className={styles.itemDescription}>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, illo ipsam nobis,     ullam omnis quibusdam voluptatem aut impedit magni assumenda perferendis debitis. Qui     totam ex officia unde nam quo molestias.
+                        </p>
+                        <button 
+                            type='submit' 
+                            className={styles.roomsButton} 
                         >
-                            HABITACIONES
-                        </Link>
-                    </button>
+                            <Link
+                                to="/habitaciones"
+                                className={styles.link}
+                            >
+                                HABITACIONES
+                            </Link>
+                        </button>
+                    </div>
+                </div>
+                <div className={styles.item2}>
+                    <div className={styles.content}>
+                        <h1 className={styles.num}>02</h1>
+                        <h2 className={styles.h2}>Disfruta las instalaciones</h2>
+                        <p className={styles.itemDescription}>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, illo ipsam nobis,     ullam omnis quibusdam voluptatem aut impedit magni assumenda perferendis debitis. Qui     totam ex officia unde nam quo molestias.
+                        </p>
+                        <button 
+                            type='submit' 
+                            className={styles.contactButton} 
+                        >
+                            <Link
+                                to="/contacto"
+                                className={styles.link}
+                            >
+                                CONTACTO
+                            </Link>
+                        </button>
+                    </div>
+                    <div className={styles.imgs}>
+                        <img className={styles.img1} src={home3} alt="home3" />
+                        <img className={styles.img2} src={home4} alt="home4" />
+                    </div>
+                </div>
+                <div className={styles.item3}>
+                    <div className={styles.imgs}>
+                        <img className={styles.img1} src={home6} alt="home6" />
+                        <img className={styles.img2} src={home5} alt="home5" />
+                    </div>
+                    <div className={styles.content}>
+                        <h1 className={styles.num}>03</h1>
+                        <h2 className={styles.look}>Mira alrededor</h2>
+                        <p className={styles.itemDescription}>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, illo ipsam nobis,     ullam omnis quibusdam voluptatem aut impedit magni assumenda perferendis debitis. Qui     totam ex officia unde nam quo molestias.
+                        </p>
+                        <button 
+                            type='submit' 
+                            className={styles.roomsButton} 
+                        >
+                            <Link
+                                to="/actividades"
+                                className={styles.link}
+                            >
+                                ACTIVIDADES
+                            </Link>
+                        </button>
+                    </div>
                 </div>
             </div>
+            <div className={styles.facilities}>
+                <h1 className={styles.titleFacilities}>INSTALACIONES</h1>
+                <p className={styles.text}>
+                    En [nombre] ofrecemos habitaciones con aire acondicionado y TV de pantalla plana por cable. También contamos con recepción 24 horas, restaurante y WiFi gratis en todas las instalaciones. Este alojamiento cuenta con salón compartido, mostrador de información turística y servicio de venta de entradas.
+                </p>
+            </div>
+            <Galery />
         </div>
     )
 }
